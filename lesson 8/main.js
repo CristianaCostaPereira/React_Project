861118380697ba5c13f2cb54f83d2eb1822dc4fb
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-10-07 11:15:43 +0100

    Components within components

---
+++ lesson 8/main.js
@@ -0,0 +1,25 @@
+Vue.component('task-list', {
+    template:
+        `<div>
+            '<task v-for="task in tasks">{{ task.task }}</task>'
+        </div>`,
+
+    data() {
+        return {
+            tasks: [
+                { task: 'Go to store', completed: true},
+                { task: 'Finish screencast', completed: false},
+                { task: 'Make donation', completed: true},
+                { task: 'Clear inbox', completed: false},
+            ]
+        }
+    }
+});
+
+Vue.component('task', {
+    template: '<li><slot></slot</li>'
+});
+
+new Vue({
+    el: '#root'
+});
