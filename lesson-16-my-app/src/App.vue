db60b9779d476fc1b68768a2f9c4751adbe72e62
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-10-09 16:48:30 +0100

    Webpack and vue-cli

---
+++ lesson-16-my-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <div id="app">
+    <message>Hello there</message>
+    <message>Hello again</message>
+  </div>
+
+</template>
+
+
+<script>
+
+  import Message from "./components/Message.vue"; // Import of components that App requires
+
+  export default {
+    name: 'app',
+
+    components: { Message }, // Nested components that App requires
+
+    data () {
+      return {
+
+      }
+    }
+  }
+
+</script>
+
+
+<style>
+
+</style>
---
+++ lesson-16-my-app/src/components/Message.vue
@@ -0,0 +1,21 @@
+<!-- Component contain in its own file !-->
+<template>
+    <div class="box">
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<script>
+    // if it had data() ou behaviour [methods()] it would be nested here
+    export default {
+    
+}
+</script>
+
+<style>
+
+    .box { background: #e3e3e3; padding: 10px; border: 1px solid #c5c5c5; margin-bottom: 10px; text-align: center;}
+
+</style>
